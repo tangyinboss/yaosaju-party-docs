ce9114c3821ab614345957388f4fa6828acad799
唐寅 <1955545766@qq.com>
2026-08-07 20:15:15 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,28 +6,28 @@ export default defineConfig({
   description: "重庆秀天文化传媒｜一机多玩线下聚会微信小程序，聚餐团建喝酒游戏集合",
 
   head: [
-    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png?v=1' }],
-    ['link', { rel: 'apple-touch-icon', href: '/favicon.png?v=1' }]
+    ['link', { rel: 'icon', type: 'image/png', href: 'favicon.png?v=1' }],
+    ['link', { rel: 'apple‑touch‑icon', href: 'favicon.png?v=1' }]
   ],
 
   themeConfig: {
-    logo: '/logo.png',
+    logo: 'logo.png',
 
     nav: [
-      { text: "首页", link: "/" },
-      { text: "游戏详情", link: "/games" },
-      { text: "使用教程", link: "/usage" },
-      { text: "常见问题", link: "/faq" }
+      { text: "首页", link: "" },
+      { text: "游戏详情", link: "games" },
+      { text: "使用教程", link: "usage" },
+      { text: "常见问题", link: "faq" }
     ],
 
     sidebar: [
       {
         text: "文档导航",
         items: [
-          { text: "首页", link: "/" },
-          { text: "游戏详情", link: "/games" },
-          { text: "使用教程", link: "/usage" },
-          { text: "常见问题", link: "/faq" }
+          { text: "首页", link: "" },
+          { text: "游戏详情", link: "games" },
+          { text: "使用教程", link: "usage" },
+          { text: "常见问题", link: "faq" }
         ]
       }
     ]
